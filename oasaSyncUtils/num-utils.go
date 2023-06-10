2bcad0d9c96920750beec7ab71f8a83151884398
Oulis Nikos <oulisnikos@gmail.com>
2023-06-10 14:08:13 +0300

    Add Utils(NumUtils, String Utils)

---
+++ oasaSyncUtils/num-utils.go
@@ -0,0 +1,42 @@
+package oasaSyncUtils
+
+import (
+	"strconv"
+	"strings"
+)
+
+func StrToInt8(input interface{}) int8 {
+	return int8(stringToNumberInternal(input.(string), 8))
+}
+
+func StrToInt64(input interface{}) int64 {
+	return stringToNumberInternal(input.(string), 64)
+}
+
+func StrToInt32(input interface{}) int32 {
+	return int32(stringToNumberInternal(input.(string), 32))
+}
+
+func StrToInt16(input interface{}) int16 {
+	return int16(stringToNumberInternal(input.(string), 16))
+}
+
+func stringToNumberInternal(input string, bitSize int) int64 {
+	sourceNumVal, error := strconv.ParseInt(strings.Trim(input, " "), 10, bitSize)
+	if error != nil {
+		panic("Δεν ήταν δυνατή η μετατροπή της συμβολοσειράς σε αριθμό για το πεδίο " + input)
+	}
+	return sourceNumVal
+}
+
+func stringToFloatInternal(input string, bitSize int) float64 {
+	sourceNumVal, error := strconv.ParseFloat(strings.Trim(input, " "), bitSize)
+	if error != nil {
+		panic("Δεν ήταν δυνατή η μετατροπή της συμβολοσειράς σε αριθμό για το πεδίο " + input)
+	}
+	return sourceNumVal
+}
+
+func StrToFloat(input interface{}) float64 {
+	return stringToFloatInternal(input.(string), 64)
+}
