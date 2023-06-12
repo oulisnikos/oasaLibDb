261d5b676b890e945cc78e4c5e0d98f6908c26b0
Oulis Nikos <oulisnikos@gmail.com>
2023-06-13 02:15:07 +0300

    Changes in BusStop struct.

--- oasaSyncModel/BusRouteStops.go
+++ oasaSyncModel/BusRouteStops.go
@@ -1,7 +1,7 @@
 package oasaSyncModel
 
 type BusRouteStops struct {
-	Route_code int64 `json:"route_code" gorm:"primaryKey"`
+	Route_code int32 `json:"route_code" gorm:"primaryKey"`
 	Stop_code  int64 `json:"stop_code" gorm:"primaryKey"`
 	Senu       int16 `json:"senu" gorm:"primaryKey"`
 }
