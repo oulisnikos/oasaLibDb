a728ed6a5c033dd9bc52eff3febb6e18d426f60b
Oulis Nikos <oulisnikos@gmail.com>
2023-06-12 23:38:27 +0300

    Logger

---
+++ logger/logger.go
@@ -0,0 +1,33 @@
+package logger
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+	"path"
+)
+
+const (
+	rootLogsDirpath = "/oasa-telematics"
+)
+
+var Logger *logrus.Logger
+
+func InitLogger(applicationName string) {
+	Logger = logrus.New()
+	err := os.Mkdir(rootLogsDirpath, 0777)
+	if err != nil {
+		fmt.Printf("error create directory file: %v\n", err)
+	}
+	fileName := path.Join(rootLogsDirpath, applicationName, "opswlog.log")
+	// open a file
+	f, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v\n", err)
+	}
+	Logger.SetOutput(f)
+}
+
+func INFO(str string) {
+	Logger.Println(str)
+}
